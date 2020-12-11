fc9f1eda11b9692ee4b2b869f7e40d7c23c97b7c
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2020-12-11 16:14:41 +0000

    Change copyright

--- DLL/Organisms/Organisms/DeadlyMimic.cs
+++ DLL/Organisms/Organisms/DeadlyMimic.cs
@@ -29,3 +29,58 @@ namespace Organisms
         public override int Life
         {
             get { return life; }
+            set { life = value; }
+        }
+        public override int Size
+        {
+            get { return size; }
+        }
+        public override int PositionX
+        {
+            get { return positionX; }
+            set { positionX = value; }
+        }
+        public override int PositionY
+        {
+            get { return positionY; }
+            set { positionY = value; }
+        }
+        public override string Name
+        {
+            get { return name; }
+        }
+        /// ******************************************** Methods ************************************
+        public DeadlyMimic(int x, int y)
+        {
+            //increase the count
+            count++;
+            life = 5;
+            size = 0;
+            PositionX = x;
+            PositionY = y;
+            name = "DeadlyMimic";
+        }   //Constructor
+        public override void Eat()
+        {
+            //if it gets food it can live for 5 generations
+            //not compounded, it just resets, it is not added
+            life = 5;
+            growth += 2;
+        }//Eat
+
+        public override void Grow()
+        {
+            if (growth > 0)
+            {
+                growth--;
+                size++;
+            }
+        }
+        public override MajesticPlant Pollinate() { return null; }
+
+        ~DeadlyMimic()
+        {
+            count--;
+        }//destructor
+    }//DeadlyMimic 
+}
