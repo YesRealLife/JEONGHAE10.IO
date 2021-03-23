54d31afb1fa704d6aa8a75b234b0dcb53581ac6a
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2021-03-23 22:39:53 +0000

    Fix timezone

--- DLL/Organisms/Organisms/Plant.cs
+++ DLL/Organisms/Organisms/Plant.cs
@@ -5,4 +5,4 @@
 // Date: 10/23/2020
 //
 // Description :
-// To create a library (dll) of cr
+// To create a library (dll) of critters; to instantiate objects of classes in an inheritance hierarchy and 
