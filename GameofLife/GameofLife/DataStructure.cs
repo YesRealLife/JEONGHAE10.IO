38b231d18edd8b7c67dbb2e77c18f74619d8854c
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2021-08-14 08:57:30 +0000

    oh my, I hate roselyn

--- GameofLife/GameofLife/DataStructure.cs
+++ GameofLife/GameofLife/DataStructure.cs
@@ -40,3 +40,78 @@ namespace GameofLife
         }
         public int FlyCount
         {
+            get { return flyCount; }
+            set { flyCount = value; }
+        }
+        public int MajesticCount
+        {
+            get { return majesticCount; }
+            set { majesticCount = value; }
+        }
+
+        public Actor[,] Actors
+        {
+            get
+            {
+                //only get, no set
+                return actors;
+            }
+        }
+        ///************************************************* Methods ***************************************************
+        public DataStructure()
+        {
+            rand = new Random();
+        }//constructor
+
+        public DataStructure(int gridSizeX, int gridSizeY)
+        {
+            actors = new Actor[gridSizeX, gridSizeY];
+            rand = new Random();
+        }//constructor
+
+        ///************************************************* Fill 2D Array *******************************************
+
+
+        public void Fill2DArray(int flyNum, int deadlyNum, int majesticNum, int gridSizeX, int gridSizeY)
+        {
+            //create array the same size as the grid
+            actors = new Actor[gridSizeX, gridSizeY];
+            Actor temp;
+            //Create the number of flys
+            for (int x = 0; x < flyNum; x++)
+            {
+                temp = new Organisms.Fly(rand.Next(gridSizeX), rand.Next(gridSizeY));
+                Flies.Add(temp);
+                //inset organism randomly into the actors 2D array
+                RandomInsert(temp, gridSizeX, gridSizeY);
+            }
+            for (int x = 0; x < deadlyNum; x++)
+            {
+                temp = new Organisms.DeadlyMimic(rand.Next(gridSizeX), rand.Next(gridSizeY));
+                RandomInsert(temp, gridSizeX, gridSizeY);
+            }
+            for (int x = 0; x < majesticNum; x++)
+            {
+                temp = new Organisms.MajesticPlant(rand.Next(gridSizeX), rand.Next(gridSizeY));
+                RandomInsert(temp, gridSizeX, gridSizeY);
+            }
+
+        }//Fill2DArray
+
+        //******************************** Load Single Actor from Picture Grid ********************
+        public void LoadSingleActorfromPictureGrid(string name, int x, int y, int gridSizeX, int gridSizeY )
+        {
+            
+           int a = int.Parse(name);
+           //switch
+            switch (a)
+            {
+                case 1:
+                    Actors[x, y] = new Organisms.Fly(x, y);
+                    break;
+                case 2:
+                    Actors[x, y] = new Organisms.DeadlyMimic(x, y);
+                    break;
+                case 3:
+                    Actors[x, y] = new Organisms.MajesticPlant(x, y);
+                    break;
