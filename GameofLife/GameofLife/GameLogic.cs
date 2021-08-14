ec03c1c1dd271c83d171f5be273636c2a4b36d6b
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2021-08-14 16:50:02 +0000

    CF 1.5

--- GameofLife/GameofLife/GameLogic.cs
+++ GameofLife/GameofLife/GameLogic.cs
@@ -36,3 +36,85 @@ namespace GameofLife
         public GameLogic()
         {
         }//constructor
+
+        public void LoadGameLogic(DataStructure D)
+        {
+            Data = D;
+        }
+
+        ///************************************************* Reset Organism Count *******************************************
+        public void ResetOrganismCount()
+        {
+            //reset all the counts to 0
+            DataArr.FlyCount = 0;
+            DataArr.MajesticCount = 0;
+            DataArr.DeadlyCount = 0;
+            //delete every thing in the fly List
+            DataArr.Flies.Clear();
+        }
+
+        ///************************************************* Update Grid *******************************************
+        public Image UpdateGrid(int x, int y, int gridSizeX, int gridSizeY)
+        {
+            Image ret = null;
+
+            if (DataArr.Actors[x, y] != null)
+            {
+                //decrement life
+                if (DataArr.Actors[x, y].Name != "MajesticPlant")
+                {
+                    DataArr.Actors[x, y].Life--;
+                }
+                if (DataArr.Actors[x, y].Life < 0)
+                {
+                    //if organism is dead remove it from the 2D object array
+                    DataArr.Actors[x, y] = null;
+                    System.GC.Collect();
+                    return null;
+                }
+                //perform the grow action
+                DataArr.Actors[x, y].Grow();
+
+                if (DataArr.Actors[x, y].Name == "MajesticPlant")
+                {
+                    if (DataArr.Actors[x, y].Size < 1)
+                    {
+                        ret = GameofLife.Properties.Resources.MajesticPlant1;
+                    }
+                    else if (DataArr.Actors[x, y].Size < 2)
+                    {
+                        ret = GameofLife.Properties.Resources.MajesticPlant2;
+                    }
+                    else
+                    {
+                        ret = GameofLife.Properties.Resources.MajesticPlant3;
+                    }
+                    //static count
+                    DataArr.MajesticCount = DataArr.Actors[x, y].Count;
+                }
+                if (DataArr.Actors[x, y].Name == "DeadlyMimic")
+                {
+                    if (DataArr.Actors[x, y].Size < 1)
+                    {
+                        ret = GameofLife.Properties.Resources.DeadlyMimic1;
+                    }
+                    else if (DataArr.Actors[x, y].Size < 2)
+                    {
+                        ret = GameofLife.Properties.Resources.DeadlyMimic2;
+                    }
+                    else
+                    {
+                        ret = GameofLife.Properties.Resources.DeadlyMimic3;
+                    }
+                    //static count
+
+                    DataArr.DeadlyCount = DataArr.Actors[x, y].Count;
+                }
+                if (DataArr.Actors[x, y].Name == "Fly")
+                {
+                    ret = GameofLife.Properties.Resources.Fly1;
+                    //add each fly back into the fly List
+                    DataArr.Flies.Add(DataArr.Actors[x, y]);
+                    //static count
+
+                    DataArr.FlyCount = DataArr.Actors[x, y].Count;
