880fcb80af25c1c06ce4de492820b17af3e9d8f6
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2021-08-17 20:13:28 +0000

    add paginator

--- GameofLife/GameofLife/MainForm.Designer.cs
+++ GameofLife/GameofLife/MainForm.Designer.cs
@@ -10,4 +10,5 @@
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherw
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
