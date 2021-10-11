f2f86ec3cbf7eea2400b6b89a0115500ab77869f
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2021-10-11 05:57:01 +0000

    Fixed Makefile

--- GameofLife/GameofLife/MainForm.Designer.cs
+++ GameofLife/GameofLife/MainForm.Designer.cs
@@ -66,4 +66,5 @@
             // 
             this.textBox_FlyNum.Location = new System.Drawing.Point(77, 14);
             this.textBox_FlyNum.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox_FlyNum.Name = "textBox_
+            this.textBox_FlyNum.Name = "textBox_FlyNum";
+            this.textBox_FlyNum.Size = new System.Drawing.Size(28, 22)
