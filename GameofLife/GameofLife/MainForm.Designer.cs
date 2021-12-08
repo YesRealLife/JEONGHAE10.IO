f6a77dd8535561a0d8f4fc0250fc67bcefae0c50
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2021-12-08 20:16:38 +0000

    add blacklist

--- GameofLife/GameofLife/MainForm.Designer.cs
+++ GameofLife/GameofLife/MainForm.Designer.cs
@@ -135,4 +135,6 @@
             this.label_Cols.AutoSize = true;
             this.label_Cols.Location = new System.Drawing.Point(167, 18);
             this.label_Cols.Name = "label_Cols";
-            this.label_Col
+            this.label_Cols.Size = new System.Drawing.Size(47, 17);
+            this.label_Cols.TabIndex = 61;
+   
