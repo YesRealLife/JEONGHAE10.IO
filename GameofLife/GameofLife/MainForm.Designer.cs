919fc0d7bc4472e0712170487aa64215a4e019e5
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2022-06-04 16:48:06 +0000

    fix email

--- GameofLife/GameofLife/MainForm.Designer.cs
+++ GameofLife/GameofLife/MainForm.Designer.cs
@@ -300,4 +300,8 @@
             this.label_genCount.Name = "label_genCount";
             this.label_genCount.Size = new System.Drawing.Size(13, 17);
             this.label_genCount.TabIndex = 70;
-            this.label_genCount.Text 
+            this.label_genCount.Text = "-";
+            // 
+            // label_generationNum
+            // 
+      
