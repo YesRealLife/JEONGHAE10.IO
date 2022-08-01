4047aac87c15752a4fb8c3b801d9f0197eb5a591
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2022-08-01 06:12:55 +0000

    update  readme

--- GameofLife/GameofLife/MainForm.Designer.cs
+++ GameofLife/GameofLife/MainForm.Designer.cs
@@ -347,4 +347,5 @@
             this.label_MajesticCount.Location = new System.Drawing.Point(657, 30);
             this.label_MajesticCount.Name = "label_MajesticCount";
             this.label_MajesticCount.Size = new System.Drawing.Size(13, 17);
-            this.label_Majestic
+            this.label_MajesticCount.TabIndex = 65;
+            this.label_MajesticCount.Text = "-";
