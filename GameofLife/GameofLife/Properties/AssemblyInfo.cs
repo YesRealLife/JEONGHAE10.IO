073914d02894fb016b05f75b33b72db29b3ec652
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2022-11-11 22:08:29 +0000

    fix new line

--- GameofLife/GameofLife/Properties/AssemblyInfo.cs
+++ GameofLife/GameofLife/Properties/AssemblyInfo.cs
@@ -17,4 +17,6 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[asse
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the
