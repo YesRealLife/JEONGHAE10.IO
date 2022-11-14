889cc594c84a2fb1445c9c3c76bc5bce3cc17f4c
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2022-11-14 17:42:29 +0000

    Updating copyright

--- GameofLife/GameofLife/Properties/AssemblyInfo.cs
+++ GameofLife/GameofLife/Properties/AssemblyInfo.cs
@@ -20,4 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e439e711-3132-436e-bb07
+[assembly: Guid("e439e711-3132-436e-bb07-e851ac57c881")]
+
+// Version information for an assembly consists of the following four values:
