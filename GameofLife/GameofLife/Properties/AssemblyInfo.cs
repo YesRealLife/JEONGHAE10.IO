ff6b8ab751994a6461eb24b2afe736eb82777aff
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2022-11-16 19:00:23 +0000

    Add i18n support

--- GameofLife/GameofLife/Properties/AssemblyInfo.cs
+++ GameofLife/GameofLife/Properties/AssemblyInfo.cs
@@ -30,4 +30,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers
-// by usi
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly
