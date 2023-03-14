97cfc2a790bb74a16f5e34939f158d8b81caa309
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2023-03-14 20:47:04 +0000

    Refactor README.md

--- GameofLife/GameofLife/Properties/Settings.Designer.cs
+++ GameofLife/GameofLife/Properties/Settings.Designer.cs
@@ -13,4 +13,4 @@ namespace GameofLife.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttr
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11
