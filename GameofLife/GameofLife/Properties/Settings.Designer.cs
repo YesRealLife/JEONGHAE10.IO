ad82cc456d77863ea03eb0fd72163eeb3f1fad2d
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2023-03-18 06:21:51 +0000

    Update SampleCode.Bas

--- GameofLife/GameofLife/Properties/Settings.Designer.cs
+++ GameofLife/GameofLife/Properties/Settings.Designer.cs
@@ -14,4 +14,7 @@ namespace GameofLife.Properties
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global:
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
+        private static Settings default
