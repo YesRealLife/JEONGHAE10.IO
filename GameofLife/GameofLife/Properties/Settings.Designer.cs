eef7eb84dc0ec9dc0d5665b169e7b2f43d388ab3
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2023-03-18 05:31:24 +0000

    Refactoring

--- GameofLife/GameofLife/Properties/Settings.Designer.cs
+++ GameofLife/GameofLife/Properties/Settings.Designer.cs
@@ -13,4 +13,5 @@ namespace GameofLife.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global:
