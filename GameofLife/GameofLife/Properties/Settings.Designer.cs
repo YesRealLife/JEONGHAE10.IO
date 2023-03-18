40937ced08a3bad17d63b403ef3b7df4ef69c32a
YesRealLife <93032838+YesRealLife@users.noreply.github.com>
2023-03-18 22:04:09 +0000

    mode

--- GameofLife/GameofLife/Properties/Settings.Designer.cs
+++ GameofLife/GameofLife/Properties/Settings.Designer.cs
@@ -17,4 +17,4 @@ namespace GameofLife.Properties
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
 
-        private static Settings default
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchroni
